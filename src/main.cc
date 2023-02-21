3f6a563c22f244225ce3c39fdd8f0f0473b3701e
Bryance Oyang <bryance@ducky>
2023-02-21 04:32:54 -0800

    eod

--- src/main.cc
+++ src/main.cc
@@ -15,15 +15,14 @@
 #include "render.h"
 #include "ws_ctube.h"
 
-
 class Broadcaster {
 public:
 	std::unique_ptr<std::thread> thread;
 	ws_ctube *ctube;
-	Scene &scene;
+	Camera &camera;
 
-	Broadcaster(Scene &scene, int port, int max_nclient, int timeout_ms, double max_broadcast_fps)
-	: scene{scene}
+	Broadcaster(Camera &camera, int port, int max_nclient, int timeout_ms, double max_broadcast_fps)
+	: camera{camera}
 	{
 		ctube = ws_ctube_open(port, max_nclient, timeout_ms, max_broadcast_fps);
 		start_thread();
@@ -37,9 +36,9 @@ public:
 	void broadcast() noexcept
 	{
 		if (ctube != NULL) {
-			std::lock_guard<std::mutex>{scene.camera.mutex};
-			ws_ctube_broadcast(ctube, scene.camera.pixel_data.data,
-				scene.camera.pixel_data.bytes());
+			std::lock_guard<std::mutex>{camera.mutex};
+			ws_ctube_broadcast(ctube, camera.pixel_data.data,
+				camera.pixel_data.bytes());
 		}
 	}
 
@@ -51,12 +50,12 @@ public:
 	void thread_main()
 	{
 		// TODO: terminate thread
-		std::unique_lock<std::mutex> mutex{scene.camera.mutex};
+		std::unique_lock<std::mutex> mutex{camera.mutex};
 		for (;;) {
-			while (!scene.camera.pixel_data_updated) {
-				scene.camera.cond.wait(mutex);
+			while (!camera.pixel_data_updated) {
+				camera.cond.wait(mutex);
 			}
-			scene.camera.pixel_data_updated = false;
+			camera.pixel_data_updated = false;
 			broadcast();
 		}
 	}
@@ -66,7 +65,7 @@ int main()
 {
 	std::vector<std::unique_ptr<RenderThread>> render_threads;
 	Scene scene;
-	Broadcaster ctube{scene, 9743, 3, 0, 24};
+	Broadcaster ctube{scene.camera, 9743, 3, 0, 24};
 
 	render_threads.emplace_back(std::make_unique<PathTracer>(0, scene, 3));
 	render_threads.emplace_back(std::make_unique<PathTracer>(2, scene, 9));
@@ -75,6 +74,5 @@ int main()
 		render_threads[i]->join();
 	}
 
-	std::terminate();
 	return 0;
 }
